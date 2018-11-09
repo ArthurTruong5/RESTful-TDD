60d4830f26223f2b0d9dd571cf7585b622b7f21b
arthurtruong <arthurtruong.work@gmail.com>
2018-11-10 01:25:10 +1100

    route tdd

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,7 @@ class ArticlesController < ApplicationController
   def index
   end
 
+  def show
+  end
+
 end
--- spec/routes/articles_spec.rb
+++ spec/routes/articles_spec.rb
@@ -4,4 +4,9 @@ describe 'article routes', type: :routing do
   it 'should route to article index' do
     expect(get '/articles').to route_to ('articles#index')
   end
+
+  it 'should route to articles show' do
+  expect(get '/articles/1').to route_to('articles#show', id: '1')
+  end
+
 end
