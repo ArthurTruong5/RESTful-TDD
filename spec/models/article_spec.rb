63315e66283e720b08dac12fb8f83954fbd27398
arthurtruong <arthurtruong.work@gmail.com>
2018-11-09 23:46:12 +1100

    models article test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,14 +9,25 @@ RSpec.describe Article, type: :model do
     it 'should validate presence of title' do
       article = build :article, title: ''
       expect(article).not_to be_valid
-      # expect(article.errors.messages[:content]).to include("can't be black")
+      expect(article.errors.messages[:title]).to include("can't be blank")
     end
 
     it 'should validate presence of content' do
       article = build :article, content: ''
       expect(article).not_to be_valid
-      # expect(article.errors.messages[:content]).to include("can't be black")
+      expect(article.errors.messages[:content]).to include("can't be blank")
     end
-  
+
+    it 'should validate presence of slug' do
+      article = build :article, slug: ''
+      expect(article).not_to be_valid
+      expect(article.errors.messages[:slug]).to include("can't be blank")
+    end
+
+    it 'should validate the uniqueness of slug' do
+      article = create :article
+      invalid_article = build :article, slug: article.slug
+      expect(invalid_article).not_to be_valid
+    end 
   end
 end
